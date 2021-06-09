123fed256981ed2cbd4ab3da738994b45ab5d5f9
Shindi Fatika Sari <66682288+ShindiFatika@users.noreply.github.com>
2021-06-09 12:30:27 +0700

    E31191120_ShindiFatikaSari_B_ API Volley

---
+++ API_Volley/app/src/main/java/com/shindifatika/api_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.shindifatika.api_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.56.1/";
+    public static final String URL_INSERT = "http://192.168.56.1/";
+    public static final String URL_DELETE = "http://192.168.56.1/";
+    public static final String URL_UPDATE = "http://192.168.56.1/";
+}
